73529636390103c4201bbca418803de184fda32c
Davin <kdavin1221@gmail.com>
2023-10-01 23:09:51 +0900

    정리

--- src/components/BackBtn.js
+++ src/components/BackBtn.js
@@ -3,10 +3,10 @@ import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { useNavigate } from "react-router-dom";
 
 
+// 뒤로가기 버튼 
 const BackBtn = () => { 
 
     const navigate = useNavigate();
-
 
     return <>
         <FontAwesomeIcon 
